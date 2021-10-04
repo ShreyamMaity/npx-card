37749ee95f8168fc0733d9738ecd58e6d8437c6b
Rey <sm8967724231@gmail.com>
2021-10-04 23:40:48 +0530

    push6

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    yourName: "                        Shreyam Maity",
+    Mail: "sm8967724231@gmail.com",
+    twitterHandle: "@ShreyamMaity",
+    workat: "Computer Science & Engineering Student at",
+    workPlace: "NSEC",
+    twitterUsername: "ShreyamMaity",
+    githubUsername: "ShreyamMaity",
+    linkedinUsername: "shreyammaity",
+    websiteUrl: "shreyam.in",
+    npxName: "Shreyam",
+};
